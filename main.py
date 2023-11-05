b285fb79e9af54f8cfcc81626e345bac8ba0fd29
Kay Gosho <ketsume0211@gmail.com>
2023-11-05 15:06:19 +0900

    feat: render results

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ import pandas as pd
 
 df = pd.read_csv('/home/kazuya/ghq/github.com/acro5piano/moncargo/frontend/machine-learning/prepared.csv', delimiter=',')
 
-X = df.iloc[:, 4:24]
+X = df
 y = df['Y']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 clf = svm.SVC()
-clf.fit(X_train, y_train)
+clf.fit(X_train.iloc[:, 4:24], y_train)
 
-predicted = clf.predict(X_test)
+predicted = clf.predict(X_test.iloc[:, 4:24])
 
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
+
+test_result = X_test.iloc[:, 0:4]
+test_result['Y_predicted'] = predicted
+print(test_result)
+
+test_result.to_csv('./result.csv')
 
